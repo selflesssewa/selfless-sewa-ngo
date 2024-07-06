c18457910876821a1ca86cb6ae002bbcd265727b
Sarthak <skalpasi@protonmail.com>
2024-07-06 19:49:30 +0530

    added receipt generation feature

--- src/app/api/pay/route.ts
+++ src/app/api/pay/route.ts
@@ -1,10 +1,17 @@
 import SHA256 from "crypto-js/sha256";
 import { nanoid } from "nanoid";
 import { NextRequest } from "next/server";
+import { PDFDocument, rgb, StandardFonts } from 'pdf-lib';
+import { readFile, writeFile, mkdir } from 'fs/promises';
+import path from 'path';
 
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
   const amount = searchParams.get("amount")!;
+  const name = searchParams.get("name")!;
+  const addr = searchParams.get("addr")!;
+  const phone = searchParams.get("phone")!;
+  const pan = searchParams.get("pan")!;
 
   const apiUrl = "https://api.phonepe.com/apis/hermes/pg/v1/pay";
   const merchantId = "M22GE2J7US8VN";
@@ -13,6 +20,91 @@ export async function GET(request: NextRequest) {
   const merchantTransactionId = nanoid();
   const merchantUserId = "MUID123";
   const redirectUrl = `https://selflesssewango.com/payment-status?t=${merchantTransactionId}&a=${amount}`;
+
+  // if user has entered their details then generate receipt
+  if (name && addr && phone && pan) {
+    const pdfPath = path.join(__dirname, '../../../../../receipt.pdf');
+    const pdfBuffer = await readFile(pdfPath);
+    const pdfDoc = await PDFDocument.load(pdfBuffer);
+    const page = pdfDoc.getPages()[0];
+    const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
+    const fontSize = 16;
+
+    const last4Chars = merchantTransactionId.slice(-4);
+    const now = new Date();
+    const month = now.getMonth() + 1;
+    const date = now.getDate();
+    
+    const textToDraw = `${month}/${last4Chars}/${date}`;
+    
+    page.drawText(textToDraw, {
+      x: 230,
+      y: 487,
+      size: fontSize,
+      font: helveticaFont,
+      color: rgb(0, 0, 0)
+    });
+
+    const timestamp = new Date().toLocaleString();
+    page.drawText(timestamp, {
+      x: 120,
+      y: 455,
+      size: fontSize,
+      font: helveticaFont,
+      color: rgb(0, 0, 0)
+    });
+
+    // Add text to the PDF in the middle of the page
+    const xStart = 150;
+    const yStart = 383;
+    const yOffset = 25;
+
+    page.drawText(`${name}`, {
+      x: xStart,
+      y: yStart,
+      size: fontSize,
+      font: helveticaFont,
+      color: rgb(0, 0, 0)
+    });
+
+    page.drawText(`${pan}`, {
+      x: xStart,
+      y: yStart - yOffset,
+      size: fontSize,
+      font: helveticaFont,
+      color: rgb(0, 0, 0)
+    });
+
+    page.drawText(`${phone}`, {
+      x: xStart,
+      y: yStart - 2*yOffset,
+      size: fontSize,
+      font: helveticaFont,
+      color: rgb(0, 0, 0)
+    });
+
+    page.drawText(`${addr}`, {
+      x: xStart,
+      y: yStart - 3*yOffset,
+      size: fontSize,
+      font: helveticaFont,
+      color: rgb(0, 0, 0)
+    });
+
+    const modifiedPdfBytes = await pdfDoc.save();
+
+    const outputPath = path.join(
+      __dirname,
+      '../../../../../receipts',
+      merchantTransactionId,
+      `${merchantTransactionId}_receipt.pdf`
+    );
+    await mkdir(path.dirname(outputPath), { recursive: true });
+    await writeFile(outputPath, modifiedPdfBytes);
+    console.log('PDF modified and saved successfully.');
+  }
+
+  return Response.json(null);
 
   const payload = {
     merchantId: merchantId,
--- src/app/donate/page.tsx
+++ src/app/donate/page.tsx
@@ -6,15 +6,53 @@ import GlowCard from "../components/GlowCard";
 
 const Page = () => {
   const [donationAmount, setDonationAmount] = useState("");
+  const [name, setName] = useState("");
+  const [contact, setContact] = useState("");
+  const [pan, setPan] = useState("");
+  const [address, setAddress] = useState("");
+  const [wantReceipt, setWantReceipt] = useState(false);
 
   const handleAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setDonationAmount(e.target.value);
   };
 
+  const handleContactChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setContact(e.target.value);
+  };
+
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
+  const handlePanChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPan(e.target.value);
+  };
+
+  const handleAddressChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAddress(e.target.value);
+  };
+
+  const handleReceiptChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setWantReceipt(e.target.checked);
+  };
+
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     if (!donationAmount) return;
-    const response = await fetch(`/api/pay?amount=${donationAmount}`);
+
+    if (wantReceipt && (!name || !contact || !pan)) {
+      alert("Please fill in all details for receipt.");
+      return;
+    }
+
+    let response = null;
+
+    if (wantReceipt) {
+      response = await fetch(`/api/pay?amount=${donationAmount}&name=${name}&phone=${contact}&addr=${address}&pan=${pan}`);  
+    } else {
+      response = await fetch(`/api/pay?amount=${donationAmount}`);
+    }
+    
     const paymentData = await response.json();
 
     if (paymentData == null) {
@@ -27,33 +65,99 @@ const Page = () => {
   return (
     <main className="min-h-screen">
       <Container className="mb-21 mt-12 flex justify-center">
-        <form onSubmit={handleSubmit} className="flex gap-4">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4 items-center">
           <GlowCard className="flex items-center gap-2 p-1 ps-3 text-title-md">
             <label htmlFor="amount">₹</label>
             <input
               type="number"
               id="amount"
               min={1}
-              placeholder="Your amount"
-              max={10_00_000}
+              placeholder="Enter Amount"
+              max={1000000}
               value={donationAmount}
               onChange={handleAmountChange}
               required
               className="min-w-[320px] rounded-[0.8rem] bg-transparent py-2 text-white focus-within:outline-none"
             />
-            <button
-              type="submit"
-              className="ms-3 flex self-center rounded-[0.8rem] bg-green-50 p-1 backdrop-blur-2xl transition-[filter,transform] duration-200 hover:scale-105 hover:saturate-150"
-            >
-              <div className="flex items-center gap-1 rounded-[0.4rem] bg-green-50 px-3 py-1">
-                <span className="text-title-md">Donate</span>
-              </div>
-            </button>
           </GlowCard>
+          <div className="flex items-center gap-2 p-1 ps-3 text-title-md">
+          <label>I hereby acknowledge that I have willingly made a donation to Selfless Sewa NGO. I understand that my contribution will go towards fulfilling the needs of those in need and for the betterment of society.</label>
+          <label htmlFor="receipt">Would you like a receipt?</label>
+            <input
+              type="checkbox"
+              id="receipt"
+              checked={wantReceipt}
+              onChange={handleReceiptChange}
+              className="rounded-[0.8rem] bg-transparent py-2 text-white focus-within:outline-none"
+            />
+          </div>
+          
+          {}
+          {wantReceipt && (
+            <>
+              <GlowCard className="flex items-center gap-2 p-1 ps-3 text-title-md">
+                <label htmlFor="name">Name</label>
+                <input
+                  type="text"
+                  id="name"
+                  placeholder="Enter Name"
+                  value={name}
+                  onChange={handleNameChange}
+                  required
+                  className="min-w-[320px] rounded-[0.8rem] bg-transparent py-2 text-white focus-within:outline-none"
+                />
+              </GlowCard>
+              <GlowCard className="flex items-center gap-2 p-1 ps-3 text-title-md">
+                <label htmlFor="contact">Contact</label>
+                <input
+                  type="tel"
+                  id="contact"
+                  placeholder="Enter Contact"
+                  value={contact}
+                  onChange={handleContactChange}
+                  required
+                  className="min-w-[320px] rounded-[0.8rem] bg-transparent py-2 text-white focus-within:outline-none"
+                />
+              </GlowCard>
+              <GlowCard className="flex items-center gap-2 p-1 ps-3 text-title-md">
+                <label htmlFor="pan">PAN Number</label>
+                <input
+                  type="text"
+                  id="pan"
+                  placeholder="Enter PAN Number"
+                  value={pan}
+                  onChange={handlePanChange}
+                  required
+                  className="min-w-[320px] rounded-[0.8rem] bg-transparent py-2 text-white focus-within:outline-none"
+                />
+              </GlowCard>
+              <GlowCard className="flex items-center gap-2 p-1 ps-3 text-title-md">
+                <label htmlFor="address">Address</label>
+                <input
+                  type="text"
+                  id="pan"
+                  placeholder="Enter Address"
+                  value={address}
+                  onChange={handleAddressChange}
+                  required
+                  className="min-w-[320px] rounded-[0.8rem] bg-transparent py-2 text-white focus-within:outline-none"
+                />
+              </GlowCard>
+            </>
+          )}
+
+          <button
+            type="submit"
+            className="mt-4 flex self-center rounded-[0.8rem] bg-green-50 p-1 backdrop-blur-2xl transition-[filter,transform] duration-200 hover:scale-105 hover:saturate-150"
+          >
+            <div className="flex items-center gap-1 rounded-[0.4rem] bg-green-50 px-3 py-1">
+              <span className="text-title-md">Donate</span>
+            </div>
+          </button>
         </form>
       </Container>
     </main>
-  );
+  );  
 };
 
 export default Page;
