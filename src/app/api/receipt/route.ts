3aba2d2ff204344c2b47ff23108d4f4ea045b1d8
sam <134122170+sahilmulla@users.noreply.github.com>
2024-07-07 13:55:39 +0530

    file path fix

--- src/app/api/receipt/route.ts
+++ src/app/api/receipt/route.ts
@@ -47,7 +47,7 @@ export async function GET(request: NextRequest) {
     modeOfPayment &&
     merchantTransactionId
   ) {
-    const pdfPath = path.join(process.cwd(), "../../../../../receipt.pdf");
+    const pdfPath = path.join(process.cwd(), "receipt.pdf");
     console.log(pdfPath);
     const pdfBuffer = await readFile(pdfPath);
     const pdfDoc = await PDFDocument.load(pdfBuffer);
